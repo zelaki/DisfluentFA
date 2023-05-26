d0b7edc8a4c55abef7e290ef65c3b9475298b030
Theodoros Kouzelis <theodoros.kouzelis@athenarc.gr>
2023-05-26 12:17:18 +0200

    fixing bugs

--- DisfluenTIMIT/syllabify/__init__.py
+++
@@ -1,4 +0,0 @@
-from syllabify.cmuparser3 import CMUtranscribe
-from .syllable_types3 import Cluster, Consonant, Vowel, Empty, Rime, Syllable  # import Py3 version
-from .phoneme_types import * 
-from syllabify import cmuparser3 
